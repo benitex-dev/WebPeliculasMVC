9fca3155d7a0deb6daba1057606e95344bff9321
benitex-dev <benitezjorgedev@gmail.com>
2025-12-14 18:34:15 -0300

    Agregar archivos de proyecto.

---
+++ Sistema-Web-Peliculas-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sistema_Web_Peliculas_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

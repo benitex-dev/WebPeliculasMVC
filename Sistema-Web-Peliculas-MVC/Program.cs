0ff05e899fbb95b73cdb65280f6628a1301acb87
benitex-dev <benitezjorgedev@gmail.com>
2025-12-16 20:20:30 -0300

    DbSeeder con configuración para su ejecución en la clase Program.cs

--- Sistema-Web-Peliculas-MVC/Program.cs
+++ Sistema-Web-Peliculas-MVC/Program.cs
@@ -12,6 +12,22 @@ builder.Services.AddDbContext<MovieDbContext>(options =>
 
 
 var app = builder.Build();
+//invocar DbSeeder
+using (var scope = app.Services.CreateScope())
+{ 
+  var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<MovieDbContext>();
+        DbSeeder.Seed(context);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred seeding the DB.");
+    }
+ 
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())

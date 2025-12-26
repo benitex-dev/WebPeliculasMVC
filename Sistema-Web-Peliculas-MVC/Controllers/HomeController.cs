f08bc5d38f8617686633e2fd452357117b698d65
benitex-dev <benitezjorgedev@gmail.com>
2025-12-26 18:56:14 -0300

    se agregan controladores  y vistas de las entidades, se agregan funcionalidad de pantalla home

--- Sistema-Web-Peliculas-MVC/Controllers/HomeController.cs
+++ Sistema-Web-Peliculas-MVC/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sistema_Web_Peliculas_MVC.Data;
 using Sistema_Web_Peliculas_MVC.Models;
 
 namespace Sistema_Web_Peliculas_MVC.Controllers
@@ -7,15 +9,18 @@ namespace Sistema_Web_Peliculas_MVC.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly MovieDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,MovieDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var peliculas = await _context.Peliculas.ToListAsync();
+            return View(peliculas);
         }
 
         public IActionResult Privacy()
